7726815b46642fa87910ba9dbd71d2babab561d7
gulbarchin <gulipr1985@gmail.com>
2025-05-13 23:42:24 +0600

    Четко!

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-# Функция-генератор
-
-def get_list():
-    for x in [1,2,3,4]:
-        yield x
-
-a = get_list()
-print(next(a))
-print(next(a))
-print(next(a))
-print(next(a))
-
-
 #Функция-итератор
 
+my_list = [1,2,3,4]
+#for i in my_list:
+#   print(i)
+
+iterator = iter(my_list)
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+
+# Эту функцию мы можем сделать с помощью while
+iterator = iter(my_list)
+try:
+    while True:
+        element = next(iterator)
+        print(element)
+except StopIteration:
+    print('The end')
 
 
